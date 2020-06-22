f121807600b7b4523ff7efbd0e67dc4783e9a054
Genghis <genghis.philip@gmail.com>
2020-06-22 17:43:49 -0500

    cleanup and let go of the dream of actually posting as user

--- mocking.py
+++ mocking.py
@@ -1,6 +1,7 @@
 import random
 import os
 import slack
+import requests
 from flask import Flask, request
 
 SLACK_TOKEN = os.environ['SLACK_TOKEN']
@@ -9,30 +10,48 @@ client = slack.WebClient(token=SLACK_TOKEN)
 app = Flask(__name__)
 
 def randomize_caps(text):
-	newstring = ':mocking: '
-	options = ['upper','lower']
-	for character in text:
-		if newstring[-1].isupper() and newstring[-2].isupper():
-			option = 'lower'
-		elif newstring[-1].islower() and newstring[-2].islower():
-			option = "upper"
-		else:
-			option = random.choice(options)
-		if option == 'upper':
-			newstring += character.upper()
-		elif option == 'lower':
-			newstring += character.lower()
-		else:
-			pass
-	newstring += ' :mocking:'
+    newstring = ':mocking: '
+    options = ['upper', 'lower']
+    for character in text:
+        if newstring[-1].isupper() and newstring[-2].isupper():
+            option = 'lower'
+        elif newstring[-1].islower() and newstring[-2].islower():
+            option = "upper"
+        else:
+            option = random.choice(options)
+        if option == 'upper':
+            newstring += character.upper()
+        elif option == 'lower':
+            newstring += character.lower()
+    newstring += ' :mocking:'
+    return newstring
 
-	return newstring
+
+def matts_inferior_caps(text):
+    newstring = ':mocking: '
+    for character in text:
+        if newstring[-1].isupper():
+            newstring += character.lower()
+        else:
+            newstring += character.upper()
+    newstring += ' :mocking:'
+    return newstring
+
 
 @app.route('/', methods=['POST'])
 def sarcastic():
-	text = request.form.get('text')
-	channel_id = request.form.get('channel_id')
-	user_id = request.form.get('user_id')
+    command = request.form.get("command")
+    text = request.form.get('text')
+    channel_id = request.form.get('channel_id')
+    user_id = request.form.get('user_id')
+    user_name = client.users_info(user=user_id)["user"]["name"]
+    icon_url = client.users_info(user=user_id)["user"]["profile"]["image_512"]
 
-	client.chat_postMessage(channel = channel_id, text = randomize_caps(text))
-	return '',200
+    if command == "mocking":
+        print(client.chat_postMessage(icon_url=icon_url, username=user_name,
+                                      channel=channel_id, text=randomize_caps(text)))
+    else:
+        print(client.chat_postMessage(icon_url=icon_url, username=user_name,
+                                      channel=channel_id, text=matts_inferior_caps(text)))
+
+    return '', 200
